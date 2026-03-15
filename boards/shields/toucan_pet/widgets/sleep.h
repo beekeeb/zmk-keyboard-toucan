e014602cff2ed0264cd0d62fe44bc379884d02b9
alee0729 <albert.c.lee@gmail.com>
2026-03-15 10:06:57 -0400

    Update sleep.h

--- boards/shields/toucan_pet/widgets/sleep.h
+++ boards/shields/toucan_pet/widgets/sleep.h
@@ -2,6 +2,18 @@
 
 #include <lvgl.h>
 
+#define SCREEN_WIDTH 144
+#define SCREEN_HEIGHT 168
+
+#define BUFFER_SIZE 168
+#define BUFFER_OFFSET_MIDDLE 0
+#define BUFFER_OFFSET_BOTTOM 0
+
+#define LVGL_BACKGROUND lv_color_white()
+#define LVGL_FOREGROUND lv_color_black()
+
+void fill_background(lv_obj_t *canvas);
+
 /**
  * Check if the sleep screen should be shown.
  * Returns true when the device is entering sleep mode.
